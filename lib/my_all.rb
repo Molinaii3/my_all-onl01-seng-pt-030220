c3c222dc0135302a0869528a45a265abe34144a5
Michael Molina <l.13molina@gmail.com>
2020-03-15 22:43:08 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,8 +4,9 @@ def my_all?(collection)
  i=0 
  block_return_values = [1,2,3]
  while i < collection.length 
-   block_return_values << yield (collection[i])
-   i = i + 1 
+   block_return_values << 
+  yield (collection[i])
+  
   end
   
   if block_return_values.include?(false)
