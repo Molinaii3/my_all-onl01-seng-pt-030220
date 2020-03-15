5d17148d779a4530cac906a8fe570a887d289037
Michael Molina <l.13molina@gmail.com>
2020-03-15 22:57:19 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,21 +1,4 @@
-require 'pry'
-
-def my_all?(collection)
- i = 0 
- block_return_values = [1,2,3]
- while i < collection.length 
-   block_return_values << 
-  yield (collection[i])
-    i = i + 1 
-  
-end
-  
-  if block_return_values.include?(false)
-    false
-  else
-    true 
-  end
-end
+require "pry"
 
 def my_all?(1,2,3)
   i = 0
